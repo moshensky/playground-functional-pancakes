e7be6f5e75a2e5bdaa833d460dd473eab45d3a8e
Nikita Moshensky <nmo@dixa.com>
2022-11-11 09:54:25 +0200

    save

--- build.sbt
+++ build.sbt
@@ -4,19 +4,19 @@ name := "functional-pancakes"
 organization := "ch.epfl.scala"
 version := "1.0"
 
-val tapirVersion = "1.2.0"
 val circeVersion = "0.14.3"
 val fs2Version = "3.3.0"
 
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats-effect" % "3.3.14",
+  "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1",
   "co.fs2" %% "fs2-core" % fs2Version,
   "co.fs2" %% "fs2-io" % fs2Version,
   "co.fs2" %% "fs2-reactive-streams" % fs2Version,
   "co.fs2" %% "fs2-scodec" % fs2Version,
-  "io.monix" %% "monix" % "3.4.1",
-  "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1",
-  "com.softwaremill.sttp.tapir" %% "tapir-core" % tapirVersion,
+  "com.softwaremill.sttp.tapir" %% "tapir-core" %  "1.2.0",
+  "com.softwaremill.sttp.shared" %% "core" % "1.3.11",
+  "com.softwaremill.sttp.model" %% "core" % "1.5.2",
+  "com.softwaremill.sttp.client3" %% "core" % "3.8.3",
   "io.circe" %% "circe-core" % circeVersion,
   "io.circe" %% "circe-generic" % circeVersion,
   "io.circe" %% "circe-generic-extras" % circeVersion,
--- src/main/scala/infra/HttpApi.scala
+++ src/main/scala/infra/HttpApi.scala
@@ -1,14 +1,25 @@
 package infra
 
-import sttp.tapir.Endpoint
+import domain._
+import sttp.tapir.{CodecFormat, Endpoint, endpoint, query, stringBody, webSocketBody}
+import fs2.{Pipe, Stream}
+import cats.effect.IO
+import sttp.capabilities.WebSockets
+
+// https://github.com/softwaremill/sttp-shared/blob/86309df2ea9c668d88f82fef073d64088b5c5921/fs2/src/main/scala/sttp/capabilities/fs2/Fs2Streams.scala
+// TODO: Where are my types!
+// import sttp.capabilities.fs2.Fs2Streams
 
 object HttpApi {
-  val pancakesEndpoint: Endpoint[Int, Strig, Pipe[
-    Task,
-    PancakeIngredient,
-    PancakeStatus
-  ], WebSockets with Fs2Streams[Task]] =
-    endpoint
-      .in("pancakes")
+  type Output = Pipe[IO, PancakeIngredient, PancakeStatus]
+  type Capabilities = WebSockets with Fs2Streams[IO]
 
+   val pancakesEndpoint: Endpoint[Int, String, Output, Capabilities] =
+     endpoint
+       .in("pancakes")
+       .in(query[Int]("pans"))
+       .errorOut(stringBody)
+       .out(webSocketBody[
+         PancakeIngredient, CodecFormat.Json,
+         PancakeStatus, CodecFormat.Json](Fs2Streams[IO]))
 }
