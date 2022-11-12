483f0c1bf7d108a837b516bcfa6cff87c32a82fa
Nikita Moshensky <nmo@dixa.com>
2022-11-12 18:50:13 +0200

    save

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,10 @@
-object Main extends App {
+import infra.Server
+import cats.effect.IO
+import cats.effect.IOApp
+import cats.effect.ExitCode
+object Main extends IOApp {
   println("Hello, World!")
+
+  def run(args: List[String]): IO[ExitCode] =
+    Server.start().use(_ => IO.never).as(ExitCode.Success)
 }
